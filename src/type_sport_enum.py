cf8fe73f93b563b4ecdab40823546cbe69cd61c3
Axel <axelcastelot@gmail.com>
2026-04-15 11:27:41 +0200

    Ajout d'une salle et des tests

---
+++ src/type_sport_enum.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class TypeSport(Enum):
+    Tennis = 0
+    Badminton = 1
+    Squash = 2
---
+++ tests/test_salle.py
@@ -0,0 +1,9 @@
+from src.salle import Salle
+from src.type_sport_enum import TypeSport
+
+def test_creation_salle():
+    salle = Salle(10, True, [], TypeSport.Tennis)
+    assert salle.capacite == 10
+    assert salle.disponibilite == True
+    assert salle.inscrits == []
+    assert salle.type_sport == TypeSport.Tennis
